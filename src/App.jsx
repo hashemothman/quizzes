0ca5aae5bbe86589f4af8c175e88634698c07aca
hashim othman <hashim993.hel993@gmail.com>
2024-03-10 18:34:04 +0300

    category1

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/quizzes' element={<Home/>}/>
         <Route path='/category' element={<Category/>}/>
       </Routes>
       <Footer/>
