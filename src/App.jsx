a68271a86a915ea5c6c4ccb4f5c6a42afdc174e0
hashim othman <hashim993.hel993@gmail.com>
2024-03-13 14:10:07 +0300

    complate

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home';
 import Category from './pages/Category/Category';
 import Footer from './components/Footer/Footer';
 import Quizpage from './pages/Quizpage/Quizpage'
+import TestPage from './pages/TestsPage/TestPage'
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,9 @@ function App() {
         <Route path='/quizzes' element={<Home/>}/>
         <Route path='/category' element={<Category/>}/>
         <Route path='/quizpage' element={<Quizpage/>}/>
+        <Route path='/category/test' element={<TestPage/>}/>
+        <Route path='/test/quizpage' element={<Quizpage/>}/>
+        <Route path='/test' element={<TestPage/>}/>
       </Routes>
       <Footer/>
     </div>
