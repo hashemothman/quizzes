52fca409c9b460b2c075bb1e4e78c68aab368059
zhraaali <zhraa002.z.ali.2002@gmail.com>
2024-03-22 18:08:33 +0300

    commit

--- src/pages/Quizpage/Quizpage.jsx
+++ src/pages/Quizpage/Quizpage.jsx
@@ -3,12 +3,15 @@ import Quiz from '../../components/Quiz/Quiz'
 import './Quizpage.css'
 import img from './../../assets/images/pedro-monteiro-HfIex7qwTlI-unsplash.jpg'
 import img2 from './../../assets/images/images.png'
+import Multichoise from '../../components/Multichoise/Multichoise'
+// import Multichoise from '../../components/Multichoise/Multichoise'
 const Quizpage = () => {
   return (
     <div className='zh-quiz-page-container'>
       <div className='zh-quiz-page'>
         <img src={img}/>
-        <Quiz/>
+        {/* <Quiz/> */}
+        <Multichoise/>
     </div>
     <img className='img-quiz' src={img2}/>
     </div>
